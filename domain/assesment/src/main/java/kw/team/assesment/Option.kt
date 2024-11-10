3a446980ec58fc357a295645bf69274d3153460c
briandr97 <briandr@naver.com>
2024-11-11 03:30:25 +0900

    feat: 실수로 삭제된 로직 복구

--- domain/assesment/src/main/java/kw/team/assesment/Option.kt
+++ domain/assesment/src/main/java/kw/team/assesment/Option.kt
@@ -6,6 +6,7 @@ data class Option(
 ) {
     companion object {
         private const val IDLE_VALUE = "아직 사용자가 선택하지 않았습니다."
+        fun getIdleOption(): Option {
             return Option(
                 value = 0,
                 content = IDLE_VALUE
